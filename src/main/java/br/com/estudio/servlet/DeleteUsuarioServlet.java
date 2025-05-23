e4480572fa413899021dd6921fb1c4fd1d4cb10d
Thiago Amaral <thiagoamaral767@gmail.com>
2025-05-23 00:42:32 -0300

    Ajustando as funcionalidades de delete

--- src/main/java/br/com/estudio/servlet/DeleteUsuarioServlet.java
+++ src/main/java/br/com/estudio/servlet/DeleteUsuarioServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/delete-usuario")
@@ -18,8 +19,20 @@ public class DeleteUsuarioServlet extends HttpServlet {
 
         new UsuarioDAO().deleteUsuarioById(usuarioId);
 
-        resp.sendRedirect("/index.jsp");
+        HttpSession session = req.getSession(false);
+        if (session != null && session.getAttribute("usuarioLogado") != null) {
+            Object usuario = session.getAttribute("usuarioLogado");
 
+
+            if (usuario instanceof br.com.estudio.model.Usuario) {
+                br.com.estudio.model.Usuario usuarioSessao = (br.com.estudio.model.Usuario) usuario;
+                if (String.valueOf(usuarioSessao.getId()).equals(usuarioId)) {
+                    session.invalidate();
+                }
+            }
+        }
+
+        resp.sendRedirect("login.jsp"); 
     }
-
 }
+
